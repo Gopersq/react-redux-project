ac5f0e7f46ed23a973c26b2a45c1b5f76c614cec
vsarkisyan <vsarkisyan@MacBook-Pro-helpdesk.local>
2025-12-08 18:24:48 +0300

    добавил структуру и сделал пару компонентов

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,18 @@ import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
-	const { theme, toggleTheme } = useTheme();
+	const { theme } = useTheme();
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Navbar />
+			<div>
+				<Sidebar />
+			</div>
 			<AppRouter />
-			<button onClick={toggleTheme}>TOGGLE</button>
 		</div>
 	);
 };
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Button.module.scss';
+import { ButtonHTMLAttributes, FC } from 'react';
+
+export enum ButtonTheme {
+	CLEAR = 'clear',
+	SECONDARY = 'secondary',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string;
+	theme?: ButtonTheme;
+}
+
+export const Button: FC<ButtonProps> = props => {
+	const { className, children, theme, ...otherProps } = props;
+
+	return (
+		<Button
+			className={classNames(cls.Button, {}, [className, cls[theme]])}
+			{...otherProps}
+		>
+			{children}
+		</Button>
+	);
+};
